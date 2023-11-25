0b50d111b61887fb1634b75308a2a6fe61601941
Aris <72753404+aristsam@users.noreply.github.com>
2023-11-26 00:35:32 +0200

    Update exercise5.js

--- exercise5.js
+++ exercise5.js
@@ -28,33 +28,22 @@ const array = [
 ];
 
 //Create an array using forEach that has all the usernames with a "!" to each of the usernames
-let forarr=[];
-const arrayfor=array.forEach(user=>forarr.push(user.username + "!"));
-console.log(forarr);
-//Create an array using map that has all the usernames with a "? to each of the usernames
+const arrayFor=[];
+const forArray=array.forEach(user=>arrayFor.push(user.username + "!"));
+console.log(arrayFor);
 
-const arraymap=array.map(user=>user.username + "!");
-console.log(arraymap);
+//Create an array using map that has all the usernames with a "? to each of the usernames
+const arrayMap=array.map(user=>user.username + "?");
+console.log(arrayMap);
+
 //Filter the array to only include users who are on team: red
-const filterar=array.filter(user=>user.team==="red");
-console.log(filterar);
+const arrayFilter=array.filter(user=>user.team === "red");
+console.log(arrayFilter);
 
 //Find out the total score of all users using reduce
-const news=array.reduce((acc,user)=>{return user.score+acc},0);
-console.log(news);
-// (1), what is the value of i?
-// (2), Make this map function pure:
-const arrayNum = [1, 2, 4, 5, 8, 9];
-const newArray = arrayNum.map((num, i) => {
-	console.log(num, i);
-	alert(num);
-	return num * 2;
-})
-const newArray = arrayNum.map((num, i) =>  num * 2);
-  console.log(num, i);
-  alert(num);
+const arrayReduce=array.reduce((acc,user)=>{return user.score+acc},0);
+console.log(arrayReduce);
 
 //BONUS: create a new list with all user information, but add "!" to the end of each items they own.
-const arraymap2 = array.map(user => { user.items = user.items.map(item => item + "!");  return user;});
-
-console.log(arraymap2);
+const newList = array.map(user=> { user.items = user.items.map(item => item + "!"); return user;});
+console.log(newList);
